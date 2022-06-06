9d4ece99eb1cadaeb8acbbc3a73633acbd9294c8
童品豪 <102658138+PINHAO-TUNG@users.noreply.github.com>
2022-06-06 15:27:16 +0800

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -141,18 +141,18 @@ ch=13
 depth=5
 xn = []
 b=tf.Variable(0.0)
-x=tf.layers.conv2d(x,ch,3,2,'same')
+x=tf.layers.conv2d(x,ch,3,1,'same')
 x=tf.layers.batch_normalization(x)
 x = tf.nn.relu(x)
 for i in range(depth):
   xn.append(x)
-  x = tf.layers.conv2d(x,ch*(2**(i+1)),3,2,'same')
+  x = tf.layers.conv2d(x,ch*(2**(i+1)),3,1,'same')
   x = tf.layers.batch_normalization(x,center=False,scale=False)+b
   x = tf.nn.relu(x)
-  x = tf.layers.conv2d(x,ch*(2**(i+1)),3,2,'same')
+  x = tf.layers.conv2d(x,ch*(2**(i+1)),3,1,'same')
   x = tf.layers.batch_normalization(x,center=False,scale=False)+b
   x = tf.nn.relu(x)
-  x = tf.layers.conv2d(x,ch*(2**(i+1)),3,2,'same')
+  x = tf.layers.conv2d(x,ch*(2**(i+1)),3,1,'same')
   x = tf.layers.batch_normalization(x,center=False,scale=False)+b
   x = tf.nn.relu(x)
   if i <depth-1:
@@ -160,10 +160,10 @@ for i in range(depth):
 for i in range(depth):
   if i>0:
     x = tf.keras.layers.UpSampling2D((2,2))(x)
-  x = tf.layers.conv2d(x,ch*(2**(depth-i-1)),3,2,'same')+xn[-i-1]
+  x = tf.layers.conv2d(x,ch*(2**(depth-i-1)),3,1,'same')+xn[-i-1]
   x = tf.layers.batch_normalization(x,center=False,scale=False)+b
   x = tf.nn.relu(x)
-out = tf.layers.conv2d(x,6,3,2,'same')
+out = tf.layers.conv2d(x,6,3,1,'same')
 outputs = out
 outputs = tf.image.resize_images(outputs, (1080, 1920))
 outputs = tf.argmax(outputs,-1)
